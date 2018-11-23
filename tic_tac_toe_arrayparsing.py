5c3b07f1542ee578a1b53421c721dc8ef3219bcd
S4t0r1 <33390640+S4t0r1@users.noreply.github.com>
2018-11-23 18:40:17 +0100

    Update tic_tac_toe_arrayparsing.py

--- tic_tac_toe_arrayparsing.py
+++ tic_tac_toe_arrayparsing.py
@@ -1,10 +1,15 @@
-
 tst = ['.', 'o', '.', '.', 'x', '.', 'x', '.' ,'x', 'x', '.', 'x', 'x', 'x', '.']
 tst2 = ['.', '.', 'x', 'x', '.']
 
+def checkNeighbors(matrix, index1=0, index2=0):
+    count = 0
+    for cell in (matrix[index1][index2+1], matrix[index1][index2-1]):
+        if cell != '.':
+            count += 1
+    return str(count)
 
-def editIndex(array, index, symbol=''):
-    newindex = index
+def editIndex(array, index1, index2, symbol=''):
+    newindex = index1
     counts, count = {}, 0
     empt_counts, emptcount = {}, 0
     for i,e in enumerate(array):
@@ -16,7 +21,7 @@ def editIndex(array, index, symbol=''):
                     count = 0
             if e == '.':
                 if array[i+1] == symbol or array[i-1] == symbol:
-                    counts[i] = "1"
+                    counts[i] = checkNeighbors(matrix, i, index2)
                     newindex = i
                 emptcount += 1
                 if array[i+1] != '.':
@@ -25,13 +30,11 @@ def editIndex(array, index, symbol=''):
         else:
             if e == '.':
                 if array[i-1] == symbol:
-                    counts[i] = "1"
+                    counts[i] = checkNeighbors(matrix, i, index2)
                     newindex = i
                 empt_counts[i] = emptcount + 1
             elif e == symbol:
                 counts[i] = count + 1
-    print("array", array)
-    print("counts", counts)
     compare = {}
     sorted_keys = sorted([k for k in counts.keys()])
     for i,indx in enumerate(sorted_keys):
@@ -43,15 +46,14 @@ def editIndex(array, index, symbol=''):
             if i < len(sorted_keys)-1:
                 nxt = counts[sorted_keys[i+1]]
                 nxt = nxt if type(nxt)==int else 0    
-            compare[indx] = prev + 1 + nxt
+            compare[indx] = prev + (int(counts[indx])+1) + nxt
     if len(compare.keys()) > 0:
         compare = {str(k)+str(v): k for k,v in compare.items() if v==max(compare.values())}
         if len(compare.keys()) == 1:
-            newindex = max(compare.values())
+            newindex = list(compare.values())[0]
         else:
             empt_counts = {str(k)+str(v): k for k,v in empt_counts.items() if v==max(empt_counts.values())}
-            if len(empt_counts.keys()) == 1:
-                newindex = max(empt_counts.values())
+            newindex = list(empt_counts.values())[0]
     return newindex
                 
 print(editIndex(tst, 0, 'x'))
