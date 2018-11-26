9c0229b125466cc2d097209e174a1e098ca4d204
S4t0r1 <33390640+S4t0r1@users.noreply.github.com>
2018-11-26 10:08:08 +0100

    Update tic_tac_toe_arrayevaluate.py

--- tic_tac_toe_arrayevaluate.py
+++ tic_tac_toe_arrayevaluate.py
@@ -24,7 +24,7 @@ def getIndx_frSubarray(prim_data, secon_data):
     eindx = sindx+(len(substr)-1)
     return (sindx, eindx)
 
-def getArrayValues(*arrays, symbol=''):
+def getArraysValues(arrays, symbol):
     arrays_values = {}
     candidate = None
     for indx, array in enumerate(arrays):
@@ -52,16 +52,18 @@ def getArrayValues(*arrays, symbol=''):
             maxvalue = max(values_.values())
             for key in values_.keys():
                 if values_[key] == maxvalue:
-                    arrays_values[indx] = array[key+1-maxvalue:key+1]
+                    arrays_values[indx if symbol!='o' else str(indx)] = array[key+1-maxvalue:key+1]
+    return arrays_values
 
-def getBestArray(arrayslst, arrays_values_dict, symbol1, symbol2):
+def getBestArrays(arrays, arrays_values, symbol):
     maxlen = max(len(v) for v in arrays_values.values())
     for k, v in arrays_values.items():
+        indx = int(k)
         vstring = "".join(e for e in v)
         vlen = len(v) + (0.5 if (vstring.startswith('.') and vstring.endswith('.')) else 0)
         if type(vlen)==float:
-            sindx, eindx = getIndx_frSubarray(arrays[k], arrays_values[k])
-            if any(x==('.'or symbol) for x in getLinearNeighbors(arrays[k], sindx, eindx)):
+            sindx, eindx = getIndx_frSubarray(arrays[indx], arrays_values[k])
+            if any(x==('.'or symbol) for x in getLinearNeighbors(arrays[indx], sindx, eindx)):
                 vlen = vlen + 1
         if vstring.count(symbol)==3:
             if (type(vlen)==int and vstring.count('.')>=1) or (type(vlen)==float and vstring.count('.')%2!=0):
@@ -70,18 +72,44 @@ def getBestArray(arrayslst, arrays_values_dict, symbol1, symbol2):
         arrays_values[k] = vstring, vlen
     if type(maxlen)==int:
         for k, v in arrays_values.items():
+            indx = int(k)
             vstring, vlen = v
             if vlen==maxlen:
-                sindx, eindx = getIndx_frSubarray(arrays[k], arrays_values[k][0])
-                if any(x==('.' or symbol) for x in (arrays[k][sindx-1] if sindx>0 else '', 
+                sindx, eindx = getIndx_frSubarray(arrays[indx], arrays_values[k][0])
+                if any(x==('.' or symbol) for x in (arrays[indx][sindx-1] if sindx>0 else '', 
                                     arrays[k][eindx+1] if eindx<len(arrays[k])+1 else '')):
                     maxlen = vlen + 1
                     arrays_values[k] = vstring, maxlen
-    print(arrays_values)
-    candidate = arrays[list(k for k,v in arrays_values.items() if v[1]==maxlen)[0]]
-    print('\n', candidate)
-    print(list(k for k,v in arrays_values.items() if v[1]==maxlen))
+    arrays_values = {k: v for k,v in arrays_values.items() if v[1]==maxlen}
+    return arrays_values, maxlen
+
+all_arrays = tst1, tst2, tst3, tst4, tst5, tst6, tst7, tst8, tst9
+
+def bestArray(arrayslst, symb1='', symb2=''):
+    arraysdict_s1 = getArraysValues(all_arrays, symb1)
+    arraysdict_s2 = getArraysValues(all_arrays, symb2)
+    best_array_s1, maxval1 = getBestArrays(all_arrays, arraysdict_s1, symb1)
+    best_array_s2, maxval2 = getBestArrays(all_arrays, arraysdict_s2, symb2)
+    maxval = maxval1 if maxval1 > maxval2 else maxval2
+    best_array = {k: v[1] for k,v in {**best_array_s1, **best_array_s2}.items() if v[1]==maxval}
+    candidate = None
+    if len(best_array.keys()) > 1:
+        for k in best_array.keys():
+            if best_array[k]==10:
+                if type(k)==str:
+                    candidate = all_arrays[int(k)]
+                    break
+                candidate = all_arrays[k]
+                break
+            else:
+                if type(k)==int:
+                    candidate = all_arrays[k]
+                    break
+                candidate = all_arrays[int(k)]
+                break
+    else:
+        candidate = all_arrays[int(list(best_array.keys())[0])]
     return candidate
 
 
-getArrayValues(tst1, tst2, tst3, tst4, tst5, tst6, tst7, tst8, tst9, symbol='x')
+print(bestArray(all_arrays, 'x', 'o'))
