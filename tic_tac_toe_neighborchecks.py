a654a9dd097844a725228db0354c5f5bb2a22270
S4t0r1 <33390640+S4t0r1@users.noreply.github.com>
2018-11-27 08:42:52 +0100

    Update tic_tac_toe_neighborchecks.py

--- tic_tac_toe_neighborchecks.py
+++ tic_tac_toe_neighborchecks.py
@@ -1,6 +1,6 @@
 
 
-def getCoordinates(max_arrayindx, arrayindx, movingindx):
+def getCoordinates(matrix, max_arrayindx, arrayindx, movingindx):
     size = len(matrix)
     rowindx, colindx = 0, 0
     if (0 <= arrayindx <= size-1):
@@ -10,18 +10,22 @@ def getCoordinates(max_arrayindx, arrayindx, movingindx):
     else:
         fulldiag1_indx = size*2
         fulldiag2_indx = fulldiag1_indx + (((max_arrayindx+1)-fulldiag1_indx)/2)
-        if arrayindx == fulldiag1_indx:
-            rowindx, colindx = movingindx, movingindx
-        elif (fulldiag1_indx < arrayindx < fulldiag2_indx):
-            diffr = arrayindx-fulldiag1_indx
-            rowindx = movingindx + (diffr if arrayindx%2!=0 else 0)
-            colindx = movingindx + (diffr in arrayindx%2==0 else 0)
+        if (fulldiag1_indx <= arrayindx < fulldiag2_indx):
+            if arrayindx == fulldiag1_indx:
+                rowindx, colindx = movingindx, movingindx 
+            else:
+                diffr = arrayindx-fulldiag1_indx
+                rowindx = movingindx + (diffr if arrayindx%2!=0 else 0)
+                colindx = movingindx + (diffr in arrayindx%2==0 else 0)
         elif arrayindx == fulldiag2_indx:
             rowindx, colindx = (size-1)-movindx, movindx
         else:
-            diffr = arrayindx-fulldiag2_indx
-            rowindx = ((size-1)-movindx) - (diffr if arrayindx%2==0 else 0)
-            colindx = movindx + (diffr if arrayindx%2!=0 else 0)
+            if arrayindx == fulldiag2_indx:
+                rowindx, colindx = (size-1)-movingindx, movingindx
+            else:
+                diffr = arrayindx-fulldiag2_indx
+                rowindx = ((size-1)-movingindx) - (diffr if arrayindx%2==0 else 0)
+                colindx = movingindx + (diffr if arrayindx%2!=0 else 0)
     return rowindx, colindx
 
 
