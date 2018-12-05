c6a239628d147ccc58b58b590d0cf2b5d0f2899e
S4t0r1 <33390640+S4t0r1@users.noreply.github.com>
2018-12-05 13:14:30 +0100

    Update sudoku_new.py

--- sudoku_new.py
+++ sudoku_new.py
@@ -3,12 +3,11 @@ def file_inpt(filename, optype, encoding):
     with open(filename, optype, encoding=encoding) as fi:
         lines = fi.readlines()
         all_data = [(l.strip() if l!='\n' else '0') for l in lines[2:]]
-        print(all_data)
         parsed_data = []
         spotted = None
-        for e in all_data:
+        for i, e in enumerate(all_data):
             if e == '0':
-                if not spotted:
+                if not spotted and not any(i==x for x in (0, len(all_data)-1)):
                     spotted = True
                     parsed_data.append('')
                 else:
@@ -41,7 +40,7 @@ def getIntSquares(datastr, ilst):
         for tripl in itmp[j:(len(datastr)//3):3]:
             for e in tripl:
                 newilst.append(e)
-    return {getIndxsKey(newilst[k-9:k]): ''.join(datastr[i] for i in newilst[k-9:k]) for k in range(9, len(datastr)+1, 9)}
+    return {getIndxsKey(newilst[k-9:k]): [datastr[i] for i in newilst[k-9:k]] for k in range(9, len(datastr)+1, 9)}
 
 def getSetforArray(array):
     return {e for e in "123456789" if e not in set(array)}
@@ -67,13 +66,15 @@ def makeDataChanges(datastr, celldict):
         if len(celldict[indx]) == 1:
             changed = True
             datastr[indx] = ''.join(celldict[indx])
+            print(celldict[indx])
     if changed:
         return datastr
 
 def getAllArraysSetsDict(all_arrays_dict, celldict):
-    return {indxs_key: [celldict[i] for i, num in enumerate(all_arrays_dict[indxs_key]) if num=='0'] for indxs_key in all_arrays_dict.keys()}
+    return {idxs: [celldict[int(idx)] for i, idx in enumerate(idxs.split(';')) if all_arrays_dict[idxs][i]=='0'] for idxs in all_arrays_dict.keys()}
 
 def getArrayNakedSets(setslst):
+    print(setslst)
     checkwith = []
     for i in range(len(setslst)):
         count = 1
@@ -84,17 +85,23 @@ def getArrayNakedSets(setslst):
                     if count == len(setslst[i]) and setslst[j] not in checkwith:
                         checkwith.append(setslst[i])
     changesets = {}
+    newsetslst = setslst
     if len(checkwith) > 0:
         for check_set in checkwith:
             for i, st in enumerate(setslst):
                 if st not in checkwith:
                     changesets[i] = {num for num in st if num not in check_set}
+                    newsetslst[i] = {num for num in st if num not in check_set}
+        print(newsetslst)
         return changesets
+    else:
+        return
 
-def applyAllNakedSets(all_arrays_dict, celldict):
-    all_arrays_sets_dict = getAllArraysSetsDict(all_arrays_dict, celldict)
+def applyAllNakedSets(all_arrays_dict, celldict, all_arrays_sets_dict=None):
+    if not all_arrays_sets_dict:
+        all_arrays_sets_dict = getAllArraysSetsDict(all_arrays_dict, celldict)
     found_naked = None
-    for indxs_key in all_arrays_sets_dict.key():
+    for indxs_key in all_arrays_sets_dict.keys():
         changes = getArrayNakedSets(all_arrays_sets_dict[indxs_key])
         if changes:
             found_naked = True
@@ -105,34 +112,40 @@ def applyAllNakedSets(all_arrays_dict, celldict):
         return (celldict, all_arrays_sets_dict)
         
 
-def processChangeAndUpdate(datastr, ilst, celldict=None, all_arrays_sets_dict=None):
+def processChangeAndUpdate(datastr, ilst, cell_dict=None, all_arrays_sets_dict=None):
+    print("processing")
     rows, cols, squares = getIntRows(datastr, ilst), getIntCols(datastr, ilst), getIntSquares(datastr, ilst)
     all_arrays_dict = {**rows, **cols, **squares}
-    if not celldict and not all_arrays_sets_dict:
+    if not cell_dict and not all_arrays_sets_dict:
+        emptcells_lst = getEmptCells(data_str)
+        if len(emptcells_lst) == 0:
+            return data_str
         cell_dict = cellsSetDict(getEmptCells(data_str), all_arrays_dict)
         if not cell_dict:
-            return False
+            return print("ERROR")
     changed_datastr = makeDataChanges(datastr, cell_dict)
     if changed_datastr:
         datastr = changed_datastr
+        print_data(datastr)
+        print('*'*15)
         return processChangeAndUpdate(datastr, ilst)
-    dicts_changes = applyAllNakedSets(all_arrays_dict, celldict)
+    dicts_changes = applyAllNakedSets(all_arrays_dict, cell_dict, all_arrays_sets_dict if all_arrays_sets_dict else None)
     if dicts_changes:
-        celldict, all_arrays_sets_dict = dicts_changes
-        return processChangeAndUpdate(datastr, ilst, celldict, all_arrays_sets_dict)
+        print("dicts changes")
+        cell_dict, all_arrays_sets_dict = dicts_changes
+        return processChangeAndUpdate(datastr, ilst, cell_dict, all_arrays_sets_dict)
     else:
-        return False
+        return data_str
         
 
-data_str = file_inpt("tst.py", 'r', encoding='utf8')
+data_str = file_inpt("tst3.py", 'r', encoding='utf8')
 print_data(data_str)
 print('*'*15)
+
 
 ilst = getIndxsData(data_str)
 while data_str.count('0') > 0:
     data_str = list(data_str)
-    rows, cols, squares = getIntRows(data_str, ilst), getIntCols(data_str, ilst), getIntSquares(data_str, ilst)
-    cell_dict = cellsSetDict(getEmptCells(data_str), {**rows, **cols, **squares})
-    data_str = makeDataChanges(data_str, cell_dict)
+    data_str = processChangeAndUpdate(data_str, ilst)
     print_data(data_str)
     print('*'*15)
